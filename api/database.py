1d26ff3892c73fc525196682a53b1274b710897d
silentmass <juha.leukkunen@gmail.com>
2024-04-28 21:58:35 +0300

    Tryin vercel deploy

--- api/database.py
+++ api/database.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
+load_dotenv(".env.development.local")
 
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+# SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("POSTGRES_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
