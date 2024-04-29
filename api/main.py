6479117521374951d9f5675e20ca97465f0932dd
silentmass <juha.leukkunen@gmail.com>
2024-04-29 11:25:51 +0300

    host

--- api/main.py
+++ api/main.py
@@ -13,8 +13,8 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000",
-    "localhost:3000",
+    "http://127.0.0.1:3000",
+    "127.0.0.1:3000",
 ]
 
 app.add_middleware(
@@ -364,4 +364,4 @@ def delete_list_description(
 
 
 if __name__ == "__main__":
-    uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api.main:app", host="127.0.0.1", port=8000, reload=True)
