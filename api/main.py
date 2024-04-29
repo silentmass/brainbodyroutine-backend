09505b7e3e20f64ddde34fb9ac78978de8053dac
silentmass <juha.leukkunen@gmail.com>
2024-04-29 11:39:49 +0300

    host 2

--- api/main.py
+++ api/main.py
@@ -364,4 +364,4 @@ def delete_list_description(
 
 
 if __name__ == "__main__":
-    uvicorn.run("api.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True)
